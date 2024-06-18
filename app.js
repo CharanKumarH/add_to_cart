a22911b3725835ced6709ebafe24565edef825c4
Charan Kumar H <charankumarh4@gmail.com>
2024-06-18 18:35:37 +0530

    added dynamic html

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const add = document.querySelector(".add");
-const sub = document.querySelector(".sub");
-const number  = document.querySelector(".number");
-const add_to_cart = document.querySelector(".add_to_cart")
+    // var add = document.querySelector(".add");
+    // var sub = document.querySelector(".sub");
+    // var number = document.querySelector(".number");
+    const add_to_cart = document.querySelector(".add_to_cart");
+    const enable_buttons = document.querySelector(".enable_buttons");
+    // console.log(sub);
+    let sum = 1;
 
-let sum = 1;
-sub.disabled = true;
+    //  --------  if (add_to_cart){
 
-let adding = add.addEventListener("click", function(){
+    let cart = add_to_cart.addEventListener("click", function () {
+    // removing add button after clicking on add button
+    // add_to_cart.parentNode.removeChild(add_to_cart);
+    //  or
     
-    sum++;
-    console.log(sum);
-    number.innerHTML = sum;
-    sub.disabled = false;
-});
+    add_to_cart.remove();
 
+    //  adding the buttons to the html
+    const newHTMLContent = `<div class="button">
+                    <button class="sub">-</button>
+                    <output class="number">1</output>
+                    <button class="add">+</button>
+                    </div>`;
+    const add_button = document.querySelector(".enable_buttons");
+    add_button.innerHTML = newHTMLContent;
 
-let subtracting = sub.addEventListener("click", function(){
-if (sum >= 2){
-    sum--;
-    number.innerHTML = sum;
-}if(sum === 1){
+    
+    let add = document.querySelector(".add");
+    let  sub = document.querySelector(".sub");
+    let  number = document.querySelector(".number");
+    
     sub.disabled = true;
-}
-console.log(sum);
-});
 
+    add.addEventListener("click", function () {
+        sub.disabled = false;
+        sum++;
+        console.log(sum);
+        number.innerHTML = sum;
+    });
 
-let cart = add_to_cart.addEventListener("click", function(){
-    sum = 1;
-    number.innerHTML = sum;
-    cart.disabled = true;
-}
-)
+    let subtracting = sub.addEventListener("click", function () {
+        if (sum >= 2) {
+        sum--;
+        number.innerHTML = sum;
+        }
+        if (sum === 1) {
+        sub.disabled = true;
+        }
+        console.log(sum);
+    });
+    });
